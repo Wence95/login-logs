13a702636102af4d475e958ff611e34cd367a71c
Pablo <pab.bp95@gmail.com>
2024-12-12 05:25:33 -0300

    changed to hospital-clinico-puq

--- js/src/admin/index.js
+++ js/src/admin/index.js
@@ -2,9 +2,9 @@ import app from 'flarum/admin/app';
 import StatisticsPage from 'flarum/admin/components/StatisticsPage';
 import LoginLogsPage from './components/LoginLogsPage';
 
-app.initializers.add('hospital-login-logs', () => {
+app.initializers.add('hospital-clinico-puq-login-logs', () => {
   app.extensionData
-    .for('hospital-login-logs')
+    .for('hospital-clinico-puq-login-logs')
     .registerPage(LoginLogsPage);
   console.log('Login Logs admin extension initialized');
 });
